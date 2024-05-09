3b440db9f84bbcc50f464f0b14141d74b08b6d24
PfaulJulian <pfaul.julian.code@gmail.com>
2024-05-09 10:42:12 +0200

    removed welcome method

--- include/JPLIB/library.h
+++ include/JPLIB/library.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#include "JPLIB/type/ix.h"
-#include "JPLIB/type/ux.h"
-#include "JPLIB/type/fx.h"
-
-#include <iostream>
-
-namespace jp {
-    auto welcome() -> void {
-        std::cout << "Welcome to the JPLIB librarey!" << std::endl;
-    }
-}
+#include "type/ix.h"
+#include "type/ux.h"
+#include "type/fx.h"
