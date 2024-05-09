32dfc87016576f5437fd7aa02baed0642c7c7490
PfaulJulian <pfaul.julian.code@gmail.com>
2024-05-09 10:32:50 +0200

    changed include directory structure

---
+++ include/JPLIB/library.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "JPLIB/type/ix.h"
+#include "JPLIB/type/ux.h"
+#include "JPLIB/type/fx.h"
--- include/library.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "type/ix.h"
-#include "type/ux.h"
-#include "type/fx.h"
