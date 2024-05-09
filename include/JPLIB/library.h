4601eb6bb7e619754336832e38850df1ff161b99
PfaulJulian <pfaul.julian.code@gmail.com>
2024-05-09 10:36:58 +0200

    added welcome method

--- include/JPLIB/library.h
+++ include/JPLIB/library.h
@@ -3,3 +3,11 @@
 #include "JPLIB/type/ix.h"
 #include "JPLIB/type/ux.h"
 #include "JPLIB/type/fx.h"
+
+#include <iostream>
+
+namespace jp {
+    auto welcome() -> void {
+        std::cout << "Welcome to the JPLIB librarey!" << std::endl;
+    }
+}
